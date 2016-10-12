fb002704975d999fa1d47b323e83afdf3d7f33b0
Zero <ilovenumberzero@gmail.com>
2016-10-12 16:58:50 +0700

    First commit

---
+++ camelCase.c
@@ -0,0 +1,26 @@
+/* Copyright (c) 2016 Zero, https://www.cognitio.tech
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this files.
+ * To deal in the Files without restriction,
+ * including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell, copies of the Files
+ *
+ * THE FILES IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,...
+ * FOR SHORT, YOU TAKE YOUR OWN RISK. */
+
+#include <stdio.h>
+
+int main(void) { 
+	
+	const int thisIsNumberFive = 5;	// thisIsNumberFive is a constant which holds value 5
+
+	int auto = 4; // Since auto is a keyword -> crash program. NOTE: remove or comment this line -> OK
+	
+	printf("Five: %d\n", thisIsNumberFive); 
+	
+	return 42; 
+
+}
+
